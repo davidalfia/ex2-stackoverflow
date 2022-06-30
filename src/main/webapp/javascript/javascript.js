1a47eb4ed64291274a830fd56275ba9e87079fe9
davidalfia <davidal@edu.hac.ac.il>
2022-06-30 22:56:47 +0300

    form

--- src/main/webapp/javascript/javascript.js
+++ src/main/webapp/javascript/javascript.js
@@ -3,14 +3,7 @@
 (function () {
 
     let questions = null;
-
-    document.addEventListener("DOMContentLoaded",function (){
-
-        questions = document.getElementById("questions");
-
-        fetchLandingPage();
-
-    })
+    let submit = null;
 
     function status(response) {
         if (response.status >= 200 && response.status < 300) {
@@ -23,20 +16,18 @@
 
     function toHtml(json){
 
-        console.log(json);
-
         let str = '';
         str+=`<div class="container">`
         json.questionStack.forEach(obj=> {
             str+=`<div>${obj.question} </div>`;
             str+= `<div>${obj.answersNumber} answers</div>`;
-            str+=`<form action="/QuestionsServlet" method="post">
-                        <input type="hidden" name="questionNumber" value="${obj.key}">
-                        <input type="submit" value="show answer">
-                   </form>`;
             str+=`<form action="/AddAnswerServlet" method="get">
                         <input type="hidden" name="questionNumber" value="${obj.key}">
                         <input type="submit" value="answer">
+                   </form>`;
+            str+=`<form action="/QuestionsServlet" method="post">
+                        <input type="hidden" name="questionNumber" value="${obj.key}">
+                        <input type="submit" value="show answer">
                    </form>`;
             str+=`<br/>`
         })
@@ -57,5 +48,13 @@
 
             })
     }
+
+
+    document.addEventListener("DOMContentLoaded",function (){
+
+        questions = document.getElementById("questions");
+        fetchLandingPage();
+
+    })
 
 })();
